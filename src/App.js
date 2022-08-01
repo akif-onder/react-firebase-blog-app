bffb9958c8a99b5ca64c77afc526b8fa062c9764
akif-onder <100137858+akif-onder@users.noreply.github.com>
2022-08-01 22:07:03 +0300

    navbar added

--- src/App.js
+++ src/App.js
@@ -2,14 +2,21 @@
 import './App.css';
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from './pages/Home';
+import Post from './pages/Post';
+import Login from './pages/Login';
 
 function App() {
   return (
     <BrowserRouter>
+    <nav>
+      <Link to='/' >Home</Link>
+      <Link to='/post' >Post</Link>
+      <Link to='/login' >Login</Link>
+    </nav>
     <Routes>
       <Route path='/' element={<Home/>} />
-
-      
+      <Route path='/post' element={<Post/>} />
+      <Route path='/login' element={<Login/>} />      
     </Routes>
     </BrowserRouter>
   );
