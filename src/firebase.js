45981f218d0b9d2bafed2961bf010e018c4022ee
akif-onder <100137858+akif-onder@users.noreply.github.com>
2022-08-01 21:29:29 +0300

    firebase configured

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCpHtq_vt7vF2dYrGEoMWN51faGNCrN7tg",
+  authDomain: "blog-app-6847b.firebaseapp.com",
+  projectId: "blog-app-6847b",
+  storageBucket: "blog-app-6847b.appspot.com",
+  messagingSenderId: "183923622745",
+  appId: "1:183923622745:web:c703229e54257a47840248"
+};
+
+
+const app = initializeApp(firebaseConfig);
+export db = getFirestore(app);
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
